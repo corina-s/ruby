ebe5e374b4aabeb756fb0fe1c7a341262fb1ba8e
corina <corina.schambacher@gmail.com>
2020-06-02 10:48:16 -0700

    big O start

---
+++ javaScript DS and Algo/big_O.js
@@ -0,0 +1,14 @@
+const nemo = ['nemo'];
+
+function findNemo(array){
+
+    for(let i = 0; i < array.length; i++){
+        if (array[i] === 'nemo'){
+            console.log('found NEMO!');
+        }
+    }
+
+}
+
+findNemo(nemo);
+// O(n)
